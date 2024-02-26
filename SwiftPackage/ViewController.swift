5976a3ab9bd197ad9e9d87f78a7b4eb20ce98bad
islamzada <vusal.islamzada.01@gmail.com>
2024-02-26 21:18:12 +0400

    Initial Commit

---
+++ SwiftPackage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPackage
+//
+//  Created by Vusal Islamzada on 26.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
